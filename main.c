784d1a837deb719a1739be9f4e1c47b7d1985388
root <root@PCdeJlia.localdomain>
2026-08-19 22:51:59 -0300

    Fechando rubrica 1 = criei a fork, testei o exec e tratei em caso de erro

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -23,18 +22,25 @@ int main(int argc, char *argv[]){
         exit(1); //Encerrar processo atual
     } 
     
-    if (pid != 0){
+    if (pid > 0){
         //Estamos no processo pai
         printf("Processo Pai PID = %d Processo pai espera pelo Filho PID = %d\n", getpid(), pid);
         wait(NULL ); //Criar filho com a CS fork, o pai vai iniciar o código e quando chegar na CA wait ele vai esperar o filho pra continuar a execução
         printf("Processo pai encerrou.\n");
     }else{ //Senão estou no processo filho
+
         printf("Processo Filho PID = %d Pai possui PID = %d\n", getpid(), getppid()); //getppid conseguir PID do processo pai
         sleep(5); //filho dormindo
-        execl("/bin/echo", "echo", "Teste marcelo me odeia", NULL); //sistema exec (pro processo filho empenhar outro processo)
-        //execl vai (substituir o processo filho pelo processo chamado exec) -> o exec mata o filho e executa um novo processo no lugar dele, por isso que o filho não dorme, ele morreu ne, ai não espera os 5 segundos.
-        sleep(5);
-        printf("Filho encerrou.\n");
+
+        //com um l: argumentos separados com virgula (passa diretamente), com v: vetor de strings (se cria, voce gera), p: permite procurar o executável nos diretórios configurados na variável PATH
+        char *args[] = {"./TestandoExec", "Marcelo", "MEODEIA", NULL};
+        execvp(args[0], args); //execvp diz : execute ./TestandoExec usando esse vetor de argumentos
+
+        perror("o exec falhou.");
+        exit(1); //encerra o processo filho
+    
+        //exec vai (substituir o processo filho pelo processo chamado exec) -> se o exec funcionar, tudo o que estiver depois do filho nçao será executado 
+
     } 
 
     return 0;
