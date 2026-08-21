3cd847b4c90dee04715636a3f9671e9393f3567f
juliatenoriocalado <jtc@cesar.school>
2026-08-20 23:28:27 -0300

    Criação de variáveis para tentar controlar os argumentos e a linha de entrada. Uso de strtok para quebrar o fgets em pedaços a partir do espaço; lendo esses espaços e adicionando na struct Comando nas devidas categorias para separar os argumentos da linha de comando; printando a struct para verificação; criação da struct comando; criação do vetor de structs para add tarefas; criando looping com while para ccaso o usuário queria sair ele digite exit; utilizando variavel de controle pra caso ser task ativar 1 caso ser run ativar dois -> a pontuar essa parte

--- main.c
+++ main.c
@@ -3,45 +3,115 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
+
+typedef struct Comando {
+    char nome[30];
+    char programa[30];
+    char argumentos[10][30];
+    int quantidadeDeArgumentos;
+} Comando;
 
 int main(int argc, char *argv[]){
 
-    //inicia o processflow
+    char linhaDeComandoEscrita[100];
+    int achouOuNao = 0;
+    int quantidadeDeTarefas = 0;
+    Comando tarefas[10];
 
-    //PID = Process IDentification => Processo de IDentificação 
-    //PID retorno < 0 : erro ao criar o processo filho
-    //PID retorno = 0 : processo filho (estado) 
-    //PID retorno > 0 = processo pai (estado)
 
-    //ISSO SOU EU ESTUDANDO NÃO É IA
+    while (achouOuNao == 0){
 
-    pid_t pid = fork(); //O retorno da função fork corresponde ao comportamento ocorrido 
+        printf("processflow> ");
 
-    if (pid < 0){
-        perror("Fork falhou.");
-        exit(1); //Encerrar processo atual
-    } 
+        fgets(linhaDeComandoEscrita, 100, stdin);
+        linhaDeComandoEscrita[strcspn(linhaDeComandoEscrita, "\n")] = '\0';
+        int comparacao = strcmp(linhaDeComandoEscrita, "exit");
+
+        if (comparacao == 0){
+            achouOuNao = 1;
+        }else{
+
+            int contadorDeEntradas = 0;
+            Comando ComandoAtual;
+            ComandoAtual.quantidadeDeArgumentos = 0;
+            char *ponteiroEntradaSerQuebrada;
+            ponteiroEntradaSerQuebrada = strtok(linhaDeComandoEscrita, " "); //aspa dupla pq lê string
+            
+            int qualComandoQueEscolheram = 0;
+
+            while(ponteiroEntradaSerQuebrada){
+               
+                printf("token: %s\n",  ponteiroEntradaSerQuebrada);
+               
+                if (contadorDeEntradas == 0){
+                    if(strcmp(ponteiroEntradaSerQuebrada, "task") == 0){
+                        qualComandoQueEscolheram = 1;
+                        tarefas[quantidadeDeTarefas] = ComandoAtual;
+                        quantidadeDeTarefas++;
+                    }
+
+                    if (strcmp(ponteiroEntradaSerQuebrada, "run") == 0){
+                        qualComandoQueEscolheram = 2;
+                        for (int i=0; i<quantidadeDeTarefas; i++){
+                            if (strcmp(tarefas[i].nome, ComandoAtual.nome) == 0){
+                                printf("tarefa na posicao %d\n", i);
+                            }
+                        }
+                        
+                    }
+
+                }if (contadorDeEntradas == 1){
+                    strcpy(ComandoAtual.nome, ponteiroEntradaSerQuebrada);
+                }if (contadorDeEntradas == 2){
+                    strcpy(ComandoAtual.programa, ponteiroEntradaSerQuebrada);
+                }if (contadorDeEntradas >= 3){
+                    strcpy(ComandoAtual.argumentos[ComandoAtual.quantidadeDeArgumentos], ponteiroEntradaSerQuebrada);
+                    ComandoAtual.quantidadeDeArgumentos++;
+                } 
+
+                contadorDeEntradas++;
+                ponteiroEntradaSerQuebrada = strtok(NULL, " ");
+                
+            }
+        
+        printf("nome: %s\n", ComandoAtual.nome);
+        printf("programa: %s\n", ComandoAtual.programa);
+        printf("quantidades de argumentos: %d\n", ComandoAtual.quantidadeDeArgumentos);
+        for (int i=0; i< ComandoAtual.quantidadeDeArgumentos; i++){
+            printf("argumento(s)[%d]: %s\n", i, ComandoAtual.argumentos[i]);
+        }
+
+        pid_t pid = fork(); //O retorno da função fork corresponde ao comportamento ocorrido 
+
+        if (pid < 0){
+            perror("Fork falhou.");
+            exit(1); //Encerrar processo atual
+        } 
+        
+        if (pid > 0){
+            //Estamos no processo pai
+            printf("Processo Pai PID = %d Processo pai espera pelo Filho PID = %d\n", getpid(), pid);
+            wait(NULL ); //Criar filho com a CS fork, o pai vai iniciar o código e quando chegar na CA wait ele vai esperar o filho pra continuar a execução
+            printf("Processo pai encerrou.\n");
+        }else{ //Senão estou no processo filho
+
+            printf("Processo Filho PID = %d Pai possui PID = %d\n", getpid(), getppid()); //getppid conseguir PID do processo pai
+            sleep(5); //filho dormindo
+
+            //com um l: argumentos separados com virgula (passa diretamente), com v: vetor de strings (se cria, voce gera), p: permite procurar o executável nos diretórios configurados na variável PATH
+            char *args[] = {"./TestandoExec", "Marcelo", "MEODEIA", NULL};
+            execvp(args[0], args); //execvp diz : execute ./TestandoExec usando esse vetor de argumentos
+
+            perror("o exec falhou.");
+            exit(1); //encerra o processo filho
+        
+            //exec vai (substituir o processo filho pelo processo chamado exec) -> se o exec funcionar, tudo o que estiver depois do filho nçao será executado 
+
+            } 
+                
+        }
+    }
     
-    if (pid > 0){
-        //Estamos no processo pai
-        printf("Processo Pai PID = %d Processo pai espera pelo Filho PID = %d\n", getpid(), pid);
-        wait(NULL ); //Criar filho com a CS fork, o pai vai iniciar o código e quando chegar na CA wait ele vai esperar o filho pra continuar a execução
-        printf("Processo pai encerrou.\n");
-    }else{ //Senão estou no processo filho
-
-        printf("Processo Filho PID = %d Pai possui PID = %d\n", getpid(), getppid()); //getppid conseguir PID do processo pai
-        sleep(5); //filho dormindo
-
-        //com um l: argumentos separados com virgula (passa diretamente), com v: vetor de strings (se cria, voce gera), p: permite procurar o executável nos diretórios configurados na variável PATH
-        char *args[] = {"./TestandoExec", "Marcelo", "MEODEIA", NULL};
-        execvp(args[0], args); //execvp diz : execute ./TestandoExec usando esse vetor de argumentos
-
-        perror("o exec falhou.");
-        exit(1); //encerra o processo filho
-    
-        //exec vai (substituir o processo filho pelo processo chamado exec) -> se o exec funcionar, tudo o que estiver depois do filho nçao será executado 
-
-    } 
-
     return 0;
 }
